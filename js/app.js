7b5cb4b204f20a94adda7c9071c75c4f3c8357c7
a5hil <ashil555anto@gmail.com>
2026-07-21 12:18:40 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('campus360', []);
+
+app.controller('dashboardController', function ($scope) {
+
+    $scope.collegeName = "campus360";
+    $scope.totalStudents = "2500"
+    $scope.totalFaculty = "94"
+    
+    $scope.students = [
+    "John Doe",
+    "Jane Smith",
+    "Michael Johnson",
+    "Emily Davis",
+    "William Brown"
+    ];
+
+    $scope.addStudent = function () {
+        $scope.totalStudents++;
+    };
+
+})
