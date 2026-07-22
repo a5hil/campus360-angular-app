8dc2f14a5fc10c56e99763787ee9134a1f5d04db
a5hil <ashil555anto@gmail.com>
2026-07-22 09:29:26 +0530

    bug fix 2

--- js/app.js
+++ js/app.js
@@ -2,22 +2,25 @@ var app = angular.module('campus360', []);
 
 app.controller('dashboardController', function ($scope) {
 
-    $scope.collegeName = "campus360";
-    $scope.totalStudents = 2500
-    $scope.totalFaculty = 94
-    
+    $scope.collegeName = "Campus360";
+    $scope.totalStudents = 2500;
+    $scope.totalFaculty = 94;
+
+    $scope.showStudents = true;
+    $scope.showDashboard = true;
+
     $scope.students = [
-    "John Doe",
-    "Jane Smith",
-    "Michael Johnson",
-    "Emily Davis",
-    "William Brown"
+        "John Doe",
+        "Jane Smith",
+        "Michael Johnson",
+        "Emily Davis",
+        "William Brown"
     ];
 
     $scope.addStudent = function () {
         $scope.totalStudents++;
-        $scope.showStudents=true;
-        $scope.showDashboard=true;
+        $scope.showStudents = true;
+        $scope.showDashboard = true;
     };
 
-})
+});
